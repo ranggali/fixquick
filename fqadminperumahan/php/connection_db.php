82d6de7c3d9e697ce7958bc0f0775f518065324d
ranggali <122335520+ranggali@users.noreply.github.com>
2025-05-06 12:20:10 +0700

    Create connection_db.php

---
+++ fqadminperumahan/php/connection_db.php
@@ -0,0 +1,15 @@
+<?php
+// db.php
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "fixquick_db"; 
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
